65b3bc622f1b1436e5d4d773bb8ca2ff93d8f2f4
s0pa-fria <130664417+s0pa-fria@users.noreply.github.com>
2023-10-05 14:44:45 -0300

    inclusão

--- Templates/header.php
+++ Templates/header.php
@@ -1,5 +1,7 @@
+<html>
+<head>
 <?php
-    require_once("herpers/url.php");
+    require_once("helpers/url.php");
 ?>
 
 <link rel="stylesheet" href="<?=$BASE_URL ?>/css/style.css">
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    include_once("templates/header.php");
+?>
+
+<h1>Olá mundo!! Bem-vindo ao PHP!!</h1>
+
+<?php
+    include_once("templates/footer.php");
+?>
