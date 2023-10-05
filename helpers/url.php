0d11d33b394c3660dbb3c1aa5e973c879d1f4f92
s0pa-fria <130664417+s0pa-fria@users.noreply.github.com>
2023-10-05 14:58:02 -0300

    k

--- helpers/url.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-//Constrói a URL base do site combinando o protocolo, nome do servidor, diretório atual e barra final.
-    $BASE_URL = "http://" .$_SERVER['SERVER_NAME'] .dirname($_SERVER['REQUEST_URI']. '?'). '/';
-?>
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include_once("templates/header.php");
-?>
-
-<h1>Olá mundo!! Bem-vindo ao PHP!!</h1>
-
-<?php
-    include_once("templates/footer.php");
-?>
