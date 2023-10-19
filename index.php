0b58b0c9eacaadb082fef356592cb5f45de47ca1
s0pa-fria <130664417+s0pa-fria@users.noreply.github.com>
2023-10-19 10:22:30 -0300

    Imagens e CSS

--- index.php
+++ index.php
@@ -17,21 +17,23 @@
     <div id="posts-container">
         <?php foreach($posts as $post); ?>
         <div class="post-box">
-            <img src="<?=BASE_URL ?>/img/<?=$post['img']?>" 
+            <img src="<?=$BASE_URL ?>/img/<?=$post['img']?>" 
             alt="<?=$post['title']?>">
 
-            <h2 class="post-title"><a href="<?=BASE_URL ?>post.php? id=<?=$post['id']?>"><?= $post['title']?>
+            <h2 class="post-title"><a href="<?=$BASE_URL ?>post.php? id=<?=$post['id']?>"><?= $post['title']?>
             </a></h2>
 
             <p class="post-description"><?= $post['description']?></p>
 
             <?php foreach($posts as $post); ?>
 
-            <div class="tags-conatiner"></div>
-
+            <div class="tags-container">
+                <?php foreach($post['tags'] as $tag): ?>
+                <a href="#"><?=$tag ?></a>
+                <?php endforeach; ?>
+            </div>
         </div>
-    </div>
-    
+    </div> 
 </main>
 
 <?php
